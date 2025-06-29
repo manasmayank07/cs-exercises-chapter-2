ff16770319da3a3c156d93d76b64a046aa4417b8
Manas Mayank <manasmishra4876@gmail.com>
2025-06-29 20:46:57 +0530

    Add  Ruler.java

---
+++ Ruler.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.StdDraw;
+
+public class Ruler {
+    public static void drawRuler(int l, double x0, double x1, double y) {
+        if (l == 0) return;
+        double xm = (x0 + x1) / 2.0;
+        StdDraw.line(xm, y, xm, y + l * 0.1);
+        drawRuler(l - 1, x0, xm, y - 0.05);
+        drawRuler(l - 1, xm, x1, y - 0.05);
+    }
+
+    public static void main(String[] args) {
+        StdDraw.setScale(0, 1);
+        drawRuler(5, 0.0, 1.0, 0.5);
+    }
+}
+
+// Add the changes:
+git add Ruler.java
+git commit -m "Add recursive Ruler program"
+git push
